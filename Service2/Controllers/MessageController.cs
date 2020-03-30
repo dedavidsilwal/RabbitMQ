c03b02e211bb9b7a345bf7d031e47062164457af
David Silwal <de.davidsilwal@hotmail.com>
2020-03-30 20:29:14 +0545

    Add project files.

---
+++ Service2/Controllers/MessageController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Service2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MessageController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get([FromServices]MessagingQueueService queueService)
+        {
+
+            return Ok(queueService.Message.ToArray());
+        }
+    }
+}
---
+++ Service2/MessagingQueueService.cs
@@ -0,0 +1,38 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service2
+{
+    public class MessagingQueueService
+    {
+        public List<string> Message { get; set; } = new List<string>();
+
+        public MessagingQueueService()
+        {
+            ConnectionFactory factory = new ConnectionFactory() { HostName = "rabbitmq", Port = 5672 };
+            factory.UserName = "guest";
+            factory.Password = "guest";
+            IConnection conn = factory.CreateConnection();
+            IModel channel = conn.CreateModel();
+            channel.QueueDeclare(queue: "hello",
+                                    durable: false,
+                                    exclusive: false,
+                                    autoDelete: false,
+                                    arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body;
+                var message = Encoding.UTF8.GetString(body);
+                Message.Add(message);
+
+            };
+            channel.BasicConsume(queue: "hello",
+                                    autoAck: true,
+                                    consumer: consumer);
+        }
+    }
+}
